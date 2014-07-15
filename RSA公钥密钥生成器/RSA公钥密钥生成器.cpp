5e05f5fe3f943ee82cc55529db962efc577f632a
Yang Fei <249893977@qq.com>
2014-07-15 10:19:36 +0800

    Update RSA公钥密钥生成器.cpp

--- RSA公钥密钥生成器/RSA公钥密钥生成器.cpp
+++ RSA公钥密钥生成器/RSA公钥密钥生成器.cpp
@@ -1,4 +1,33 @@
-// RSAπ´‘ø√‹‘ø…˙≥…∆˜.cpp : ∂®“Âøÿ÷∆Ã®”¶”√≥Ã–Úµƒ»Îø⁄µ„°£
+/*
+ *  RSAÂÖ¨Èí•ÂØÜÈí•ÁîüÊàêÂô®.cpp
+ *
+ *  Created by Leslie Yang on 07/15/14.
+ *  Copyright 2014 https://github.com/LeslieFire All rights reserved.
+ *
+ *  Redistribution and use in source and binary forms, with or without
+ *  modification, are permitted provided that the following conditions are met:
+ *
+ *     * Redistributions of source code must retain the above copyright
+ *       notice, this list of conditions and the following disclaimer.
+ *     * Redistributions in binary form must reproduce the above copyright
+ *       notice, this list of conditions and the following disclaimer in the
+ *       documentation and/or other materials provided with the distribution.
+ *     * Neither the name of the author nor the names of its contributors
+ *       may be used to endorse or promote products derived from this software
+ *       without specific prior written permission.
+ *
+ *  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ *  AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+ *  THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ *  ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+ *  FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ *  (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS
+ *  OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY
+ *  OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE
+ *  OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED
+ *  OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ *  ************************************************************************************ */
 //
 #include <stdio.h>
 #include <iostream>
@@ -22,12 +51,12 @@ using namespace std;
 int main()
 {
 	Rsa rsa;
-	cout << "’˝‘⁄…˙≥…1024ŒªRSA√‹‘øŒƒº˛∫Õπ´‘øŒƒº˛...." << endl;
+	cout << "ÔøΩÔøΩÔøΩÔøΩÔøΩÔøΩÔøΩÔøΩ1024ŒªRSAÔøΩÔøΩ‘øÔøΩƒºÔøΩÔøΩÕπÔøΩ‘øÔøΩƒºÔøΩ...." << endl;
 	long t1 = clock();
 	rsa.init(512);
 	long t2 = clock();
-	cout << "RSA√‹‘øπ´‘ø…˙≥…ÕÍ≥…." << endl;
-	cout << "”√ ±:" << (t2 - t1) / 1000 << "s." << endl;
+	cout << "RSAÔøΩÔøΩ‘øÔøΩÔøΩ‘øÔøΩÔøΩÔøΩÔøΩÔøΩÔøΩÔøΩÔøΩ." << endl;
+	cout << "ÔøΩÔøΩ ±:" << (t2 - t1) / 1000 << "s." << endl;
 
 	getchar();
 	return 0;
